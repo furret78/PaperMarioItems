b468461f4f1fdfdc97540282347b8a2ea9ddf3c1
Furret78 <68061718+furret78@users.noreply.github.com>
2024-09-10 11:42:43 +0700

    minor Cookbook spawn rate changes

--- Common/ChestLoot.cs
+++ Common/ChestLoot.cs
@@ -11,16 +11,16 @@ namespace PaperMarioItems.Common
         {
             int itemsPlaced = 0;
             float maxItems = 80;
-            float spawnRate = 10;
+            float spawnRate = 12;
             if (Main.expertMode && !Main.masterMode)
             {
-                maxItems *= 0.5f;
+                maxItems *= 1.5f;
                 spawnRate *= 0.5f;
             }
             if (Main.masterMode)
             {
-                maxItems *= 0.2f;
-                spawnRate *= 0.2f;
+                maxItems *= 1.8f;
+                spawnRate *= 0.3f;
             }
             for (int chestIndex = 0; chestIndex < Main.maxChests; chestIndex++)
             {
@@ -31,15 +31,13 @@ namespace PaperMarioItems.Common
                 }
                 Tile chestTile = Main.tile[chest.x, chest.y];
                 if (chestTile.TileType == TileID.Containers &&
-                    (chestTile.TileFrameX == 16 * 36 ||
-                    chestTile.TileFrameX == 13 * 36 ||
-                    chestTile.TileFrameX == 17 * 36 ||
-                    chestTile.TileFrameX == 12 * 36 ||
-                    chestTile.TileFrameX == 3 * 36))
+                    (chestTile.TileFrameX == 16 * 36 || chestTile.TileFrameX == 13 * 36 ||
+                    chestTile.TileFrameX == 17 * 36 || chestTile.TileFrameX == 12 * 36 ||
+                    (Main.expertMode && (chestTile.TileFrameX == 1 * 36 || chestTile.TileFrameX == 3 * 36))))
                 {
-                    if (!WorldGen.genRand.NextBool((int)spawnRate))
+                    if (WorldGen.genRand.NextBool((int)spawnRate))
                         continue;
-                    else for (int inventoryIndex = 0; inventoryIndex < Chest.maxItems; inventoryIndex++)
+                    for (int inventoryIndex = 0; inventoryIndex < Chest.maxItems; inventoryIndex++)
                     {
                         if (chest.item[inventoryIndex].type == ItemID.None)
                         {
